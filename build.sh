0df3577f8ed79cf7c7fc95a9fc09ea9837d8dbd0
zhoujian <zhoujian2030@163.com>
2015-12-19 22:44:49 +0800

    add test for strtok and strtok_r

--- build.sh
+++ build.sh
@@ -12,10 +12,12 @@ then
     make -C cpp clean
     make -C test/ipc clean
     make -C test/log clean
+    make -C test/str clean
 else
     make -C log
     make -C ipc
     make -C cpp
     make -C test/ipc
     make -C test/log
+    make -C test/str
 fi
---
+++ test/str/src/StrMain.cpp
@@ -0,0 +1,44 @@
+/*
+ * StrMain.cpp
+ *
+ *  Created on: Dec 15, 2015
+ *      Author: z.j
+ */
+
+#include <string.h>
+#include <string>
+#include "CPPLogger.h"
+
+using namespace std;
+using namespace logcpp;
+
+int main() {
+    //char str[] = "This is. sample .string.";
+    string str = "This is. sample .string.";
+    char* token;
+
+    log4cplus::Logger logger = CPPLogger::getLogger("StringTest");
+    CPPLogger::initConsoleLog(logger);
+    // CPPLogger::setLogLevel(logger, INFO);
+
+    LOG4CPLUS_INFO(logger, "Spliting with strtok: \"" << str << "\", length = " << str.length());
+    token = strtok((char*)str.c_str(), " .");
+    while (token != NULL) {
+        LOG4CPLUS_INFO(logger, token);
+        LOG4CPLUS_DEBUG(logger, "str = " << str);
+        token = strtok(NULL, " .");
+    }
+    LOG4CPLUS_INFO(logger, "+++++++++++++++++++++++++++++++++++++++++++++");
+
+    char* saveptr;
+    str = "This is. sample .string.";
+    LOG4CPLUS_INFO(logger, "Spliting with strtok_r: \"" << str << "\", length = " << str.length());
+    token = strtok_r((char*)str.c_str(), " .", &saveptr);
+    while (token != NULL) {
+        LOG4CPLUS_INFO(logger, token);
+        LOG4CPLUS_DEBUG(logger, "saveptr: " << saveptr << "   str = " << str);
+        token = strtok_r(NULL, " .", &savePtr);
+    }
+
+    return 0;
+}
